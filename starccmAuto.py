420285845e6880e4a77c3cf62e9c423bf74945ed
mike <mjlaufer103@gmail.com>
2017-09-07 13:33:48 +0300

    test 1

--- starccmAuto.py
+++ starccmAuto.py
@@ -1,68 +1,78 @@
-#Starccm Batch Automation Script with PB
-#Need to install Python 3, Expect module
+# test
+# Starccm Batch Automation Script with PB
+# Need to install Python 3, Expect module
 
-import sys, os, glob, time, subprocess
+import sys
+import os
+import glob
+import time
+import subprocess
 
 # Parameters
     starccmcommand = "starccm"
     defuserpswd = "ChangeToYOURS"
     pushbullettoken = "ChangeToYOURS"
     numofcores = "4"
-    macrofile = "meshandrun.java" #empty string "" if no macrofile is needed
+    macrofile = "meshandrun.java"  # empty string "" if no macrofile is needed
 
-#function to make files executable
+# function to make files executable
+
+
 def make_executable(path):
     mode = os.stat(path).st_mode
     mode |= (mode & 0o444) >> 2
     os.chmod(path, mode)
 
-#user password can be passed as argument (or default)
-if len(sys.argv) > 1:
-	userpswd = sys.argv[1]
-else:
-	userpswd = defuserpswd
 
-#current directory
+# user password can be passed as argument (or default)
+if len(sys.argv) > 1:
+    userpswd = sys.argv[1]
+else:
+    userpswd = defuserpswd
+
+# current directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-#check files are in place
+# check files are in place
 if macrofile != "":
     macrodir = dir_path + "/" + macrofile
     if os.path.isfile(macrodir) == False:
-        print ("Error: Specified macro file not found in directory")
+        print("Error: Specified macro file not found in directory")
         sys.exit()
     macrodir = macrodir + " "
 else:
     macrodir = ""
 
-#list of .sim files
+# list of .sim files
 simfulldir = []
 simfiles = glob.glob('*.sim')
 numofsim = len(simfiles)
 for simfile in simfiles:
-	simfulldir.append(dir_path + '/' + simfile)
+    simfulldir.append(dir_path + '/' + simfile)
 if simfiles == []:
-    print ("Error: No sim files found")
+    print("Error: No sim files found")
     sys.exit()
 
-#expect script
+# expect script
 with open('expectscript.exp', 'w') as f:
-	f.write(r'#!/usr/bin/expect' + "\nset timeout -1\nspawn ./batch.sh")
-	for i in range(numofsim):
-	       f.write("\n" + r'expect "password:" {send "'+ userpswd +r'\r"}')
-	f.write("\ninteract")
+    f.write(r'#!/usr/bin/expect' + "\nset timeout -1\nspawn ./batch.sh")
+    for i in range(numofsim):
+        f.write("\n" + r'expect "password:" {send "' + userpswd + r'\r"}')
+    f.write("\ninteract")
 
-#bash script
+# bash script
 with open('batch.sh', 'w') as f:
     f.write(r'#!/bin/bash' + "\n")
     for i, sim in enumerate(simfulldir):
-        f.write(starccmcommand + " -batch -power -np " + numofcores + " " + macrodir + sim + " -podkey lM66SSsU2JDB0BfHbD8gFA" +"\n")
-        f.write("curl -u " + pushbullettoken + ": https://api.pushbullet.com/v2/pushes -d type=note -d title=\"" + simfiles[i] + " finished\"\n" )
+        f.write(starccmcommand + " -batch -power -np " + numofcores +
+                " " + macrodir + sim + " -podkey lM66SSsU2JDB0BfHbD8gFA" + "\n")
+        f.write("curl -u " + pushbullettoken +
+                ": https://api.pushbullet.com/v2/pushes -d type=note -d title=\"" + simfiles[i] + " finished\"\n")
 
-#makes batch and expect script executable
+# makes batch and expect script executable
 make_executable(dir_path + r'/batch.sh')
 make_executable(dir_path + r'/expectscript.exp')
 
-#run simulations expect--> bash --> starccm
+# run simulations expect--> bash --> starccm
 time.sleep(3)
 subprocess.call(r'./expectscript.exp', shell=True)
